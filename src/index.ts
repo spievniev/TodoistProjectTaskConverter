305f28042fe4af014c40b351cbef8821fef6a357
Serhii Pievniev <spievniev@gmail.com>
2026-05-18 20:12:07 -0400

    fix: catch unhandled rejections and handler errors

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,19 @@ import auth from "./middleware/auth";
 import toTask from "./handlers/to_task";
 import toProject from "./handlers/to_project";
 
-process.on("uncaughtException", (error) => log("Uncaught exception: " + JSON.stringify(error)));
-
 const app = new Hono().use(auth);
 
 app.post("/to_task", toTask);
 app.post("/to_project", toProject);
 
-app.notFound(async (c) => c.text("Not Found", 404));
+app.notFound((c) => c.text("Not Found", 404));
+
+process.on("uncaughtException", (error) => log("Uncaught exception: " + JSON.stringify(error)));
+process.on("unhandledRejection", (error) => log("Unhandled rejection: " + JSON.stringify(error)));
+
+app.onError((error, c) => {
+    log("Uncaught exception: " + JSON.stringify(error));
+    return c.text("Internal Server Error", 500);
+});
 
 export default app;
